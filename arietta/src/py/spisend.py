a8fe1f2b079c1938bf2fbd9c6a0f01eb8fad6e2d
Juergen Weigert <jnweiger@gmail.com>
2007-01-01 03:02:33 +0100

    first good light. Timing is reversed, when using set_leds, better when done manual, but both apear to work at 1.6 us per bit.

--- arietta/src/py/spisend.py
+++ arietta/src/py/spisend.py
@@ -15,6 +15,8 @@ class spibus():
 	read_buffer=create_string_buffer(nleds*24)
 
 		# speed_hz=5000000,
+		# speed_hz=2550000,  # 1.6us @ 4bit per bit
+		# speed_hz=3500000,    # 1.2us @ 4bit per bit
 	ioctl_arg = spi_ioc_transfer(
 		tx_buf=addressof(write_buffer),
 		rx_buf=addressof(read_buffer),
@@ -37,38 +39,52 @@ class spibus():
 #Open the SPI bus 0
 spibus0 = spibus("/dev/spidev32766.1")
 
-#Send 24 characters
+def set_led(j, r, g, b):
+  doublebit = [ 
+    # uart sends the lsb first, and we invert all
+    (255-0x11), 	# 1...1...	0 0
+    (255-0x71), 	# 1...111.	0 1
+    (255-0x17), 	# 111.1...	1 0
+    (255-0x77), 	# 111.111.	1 1
+  ]
 
-for i in range(nleds):
-  j=i*24
-  spibus0.write_buffer[ 0+j]=chr(0x55)
-  spibus0.write_buffer[ 1+j]=chr(0x55)
-  spibus0.write_buffer[ 2+j]=chr(0x55)
-  spibus0.write_buffer[ 3+j]=chr(0x55)
-  spibus0.write_buffer[ 4+j]=chr(0x55)
-  spibus0.write_buffer[ 5+j]=chr(0x55)
-  spibus0.write_buffer[ 6+j]=chr(0x55)
-  spibus0.write_buffer[ 7+j]=chr(0x55)
-  
-  spibus0.write_buffer[ 8+j]=chr(0x33)
-  spibus0.write_buffer[ 9+j]=chr(0x33)
-  spibus0.write_buffer[10+j]=chr(0x33)
-  spibus0.write_buffer[11+j]=chr(0x33)
-  spibus0.write_buffer[12+j]=chr(0x33)
-  spibus0.write_buffer[13+j]=chr(0x33)
-  spibus0.write_buffer[14+j]=chr(0x33)
-  spibus0.write_buffer[15+j]=chr(0x33)
-  
-  spibus0.write_buffer[16+j]=chr(0xAA)
-  spibus0.write_buffer[17+j]=chr(0xAA)
-  spibus0.write_buffer[18+j]=chr(0xAA)
-  spibus0.write_buffer[19+j]=chr(0xAA)
-  spibus0.write_buffer[20+j]=chr(0xAA)
-  spibus0.write_buffer[21+j]=chr(0xAA)
-  spibus0.write_buffer[22+j]=chr(0xAA)
-  spibus0.write_buffer[23+j]=chr(0xAA)
+  i = j * 12
 
-spibus0.send(24*nleds)
+  spibus0.write_buffer[i+ 0] = chr(doublebit[(g>>6) & 3])
+  spibus0.write_buffer[i+ 1] = chr(doublebit[(g>>4) & 3])
+  spibus0.write_buffer[i+ 2] = chr(doublebit[(g>>2) & 3])
+  spibus0.write_buffer[i+ 3] = chr(doublebit[(g>>0) & 3])
+
+  spibus0.write_buffer[i+ 4] = chr(doublebit[(r>>6) & 3])
+  spibus0.write_buffer[i+ 5] = chr(doublebit[(r>>4) & 3])
+  spibus0.write_buffer[i+ 6] = chr(doublebit[(r>>2) & 3])
+  spibus0.write_buffer[i+ 7] = chr(doublebit[(r>>0) & 3])
+
+  spibus0.write_buffer[i+ 8] = chr(doublebit[(b>>6) & 3])
+  spibus0.write_buffer[i+ 9] = chr(doublebit[(b>>4) & 3])
+  spibus0.write_buffer[i+10] = chr(doublebit[(b>>2) & 3])
+  spibus0.write_buffer[i+11] = chr(doublebit[(b>>0) & 3])
+
+set_led(0, 1  , 1  , 1)
+set_led(1, 1  , 1  , 20)
+set_led(2, 1  , 1  , 30)
+set_led(3, 255, 255, 0  )
+set_led(4, 255, 0  , 0  )
+set_led(5, 0  , 255, 0  )
+set_led(6, 255, 0  , 255)
+set_led(7,   5,   5, 255)
+set_led(8,   5,   5, 255)
+
+
+gn = [ 0x77, 0x77, 0x77, 0x71,  0x77, 0x77, 0x77, 0x77,   0x77, 0x77, 0x77, 0x77 ]
+rd = [ 0x77, 0x77, 0x77, 0x77,  0x77, 0x77, 0x77, 0x71,   0x77, 0x77, 0x77, 0x77 ]
+bl = [ 0x77, 0x77, 0x77, 0x77,  0x77, 0x77, 0x77, 0x77,   0x77, 0x77, 0x77, 0x71 ]
+
+for j in range(12): spibus0.write_buffer[12*0+j] = chr(gn[j])
+# for j in range(12): spibus0.write_buffer[12*1+j] = chr(bl[j])
+# for j in range(12): spibus0.write_buffer[12*2+j] = chr(rd[j])
+
+spibus0.send(3*4*9)	# nleds)
 
 #Shows the 2 byte received in full duplex in hex format
 print hex(ord(spibus0.read_buffer[0]))

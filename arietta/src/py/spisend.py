a9a3e94081a0dbf557af32fb9a1b8d18c011f8bf
Juergen Weigert <jnweiger@gmail.com>
2007-01-01 02:20:36 +0100

    blinkenlight!

--- arietta/src/py/spisend.py
+++ arietta/src/py/spisend.py
@@ -7,11 +7,12 @@ from fcntl import ioctl
 from spi_ctypes import *
 import time
 
+nleds=8
 
 class spibus():
 	fd=None
-	write_buffer=create_string_buffer(50)
-	read_buffer=create_string_buffer(50)
+	write_buffer=create_string_buffer(nleds*24)
+	read_buffer=create_string_buffer(nleds*24)
 
 		# speed_hz=5000000,
 	ioctl_arg = spi_ioc_transfer(
@@ -37,34 +38,37 @@ class spibus():
 spibus0 = spibus("/dev/spidev32766.1")
 
 #Send 24 characters
-spibus0.write_buffer[ 0]=chr(0x55)
-spibus0.write_buffer[ 1]=chr(0x55)
-spibus0.write_buffer[ 2]=chr(0x55)
-spibus0.write_buffer[ 3]=chr(0x55)
-spibus0.write_buffer[ 4]=chr(0x55)
-spibus0.write_buffer[ 5]=chr(0x55)
-spibus0.write_buffer[ 6]=chr(0x55)
-spibus0.write_buffer[ 7]=chr(0x55)
 
-spibus0.write_buffer[ 8]=chr(0x33)
-spibus0.write_buffer[ 9]=chr(0x33)
-spibus0.write_buffer[10]=chr(0x33)
-spibus0.write_buffer[11]=chr(0x33)
-spibus0.write_buffer[12]=chr(0x33)
-spibus0.write_buffer[13]=chr(0x33)
-spibus0.write_buffer[14]=chr(0x33)
-spibus0.write_buffer[15]=chr(0x33)
+for i in range(nleds):
+  j=i*24
+  spibus0.write_buffer[ 0+j]=chr(0x55)
+  spibus0.write_buffer[ 1+j]=chr(0x55)
+  spibus0.write_buffer[ 2+j]=chr(0x55)
+  spibus0.write_buffer[ 3+j]=chr(0x55)
+  spibus0.write_buffer[ 4+j]=chr(0x55)
+  spibus0.write_buffer[ 5+j]=chr(0x55)
+  spibus0.write_buffer[ 6+j]=chr(0x55)
+  spibus0.write_buffer[ 7+j]=chr(0x55)
+  
+  spibus0.write_buffer[ 8+j]=chr(0x33)
+  spibus0.write_buffer[ 9+j]=chr(0x33)
+  spibus0.write_buffer[10+j]=chr(0x33)
+  spibus0.write_buffer[11+j]=chr(0x33)
+  spibus0.write_buffer[12+j]=chr(0x33)
+  spibus0.write_buffer[13+j]=chr(0x33)
+  spibus0.write_buffer[14+j]=chr(0x33)
+  spibus0.write_buffer[15+j]=chr(0x33)
+  
+  spibus0.write_buffer[16+j]=chr(0xAA)
+  spibus0.write_buffer[17+j]=chr(0xAA)
+  spibus0.write_buffer[18+j]=chr(0xAA)
+  spibus0.write_buffer[19+j]=chr(0xAA)
+  spibus0.write_buffer[20+j]=chr(0xAA)
+  spibus0.write_buffer[21+j]=chr(0xAA)
+  spibus0.write_buffer[22+j]=chr(0xAA)
+  spibus0.write_buffer[23+j]=chr(0xAA)
 
-spibus0.write_buffer[16]=chr(0xAA)
-spibus0.write_buffer[17]=chr(0xAA)
-spibus0.write_buffer[18]=chr(0xAA)
-spibus0.write_buffer[19]=chr(0xAA)
-spibus0.write_buffer[20]=chr(0xAA)
-spibus0.write_buffer[21]=chr(0xAA)
-spibus0.write_buffer[22]=chr(0xAA)
-spibus0.write_buffer[23]=chr(0xAA)
-
-spibus0.send(24)
+spibus0.send(24*nleds)
 
 #Shows the 2 byte received in full duplex in hex format
 print hex(ord(spibus0.read_buffer[0]))

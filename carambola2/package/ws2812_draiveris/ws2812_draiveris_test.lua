f81f053709844323997f52d12291f4cd1c5583cc
Jürgen Weigert <jnweiger@gmail.com>
2015-01-02 03:07:20 +0100

    ported ws2812_draiveris_test.py to lua. we don't have enough flash for python-mini. It is 1.3MB, while lua is only 6k+30k for the socket library. I need luasocket only for its sleep() function.

---
+++ carambola2/package/ws2812_draiveris/ws2812_draiveris_test.lua
@@ -0,0 +1,64 @@
+#! /usr/bin/lua
+--
+-- Translated from python to lua. See ws2812_draiveris_test.py
+-- (C) 2015, juewei@fablab.com
+--
+
+bit=require('bit')
+socket=require('socket')
+
+os.execute("insmod ws2812-draiveris gpios=7,14,15 inverted=1")
+dev = io.open("/dev/ws2812", "wb")
+dev:setvbuf("no")
+
+function rep3(r,g,b, n)
+  if n <= 0 then return "" end
+  s = string.char(r,g,b)
+  ss = ""
+  for i = 1, n do
+    ss = ss .. s
+  end
+  return ss
+end
+
+width=30
+val=5
+v_2=bit.rshift(val,2)
+
+while true do
+
+  for i = 0, width-1 do
+    data =         rep3(1,1,0, i) .. rep3(val,0,0, 1) .. rep3(1,1,0, width-1-i)
+    data = data .. rep3(1,1,0, i) .. rep3(0,val,0, 1) .. rep3(1,1,0, width-1-i)
+    data = data .. rep3(1,1,0, i) .. rep3(0,0,val, 1) .. rep3(1,1,0, width-1-i)
+
+    data = data .. rep3(0,0,3, i)         .. rep3(0,val,val, 1) .. rep3(0,0,3, width-1-i)
+    data = data .. rep3(0,0,3, width-1-i) .. rep3(val,0,val, 1) .. rep3(0,0,3, i)
+    data = data .. rep3(0,0,3, i)         .. rep3(val,val,0, 1) .. rep3(0,0,3, width-1-i)
+
+    data = data .. rep3(0,3,0, i) .. rep3(v_2,v_2,v_2, 1)       .. rep3(0,3,0, width-1-i)
+    data = data .. rep3(0,3,0, i) .. rep3(val,val,val, 1)       .. rep3(0,3,0, width-1-i)
+    data = data .. rep3(0,3,0, i) .. rep3(width-i,width-i,i, 1) .. rep3(0,3,0, width-1-i)
+    dev:write(data)
+    socket.sleep(0.02)
+  end
+
+  for i = 0, width-1 do
+    data =         rep3(1,1,0, width-1-i) .. rep3(val,0,0, 1) .. rep3(1,1,0, i)
+    data = data .. rep3(1,1,0, width-1-i) .. rep3(0,val,0, 1) .. rep3(1,1,0, i)
+    data = data .. rep3(1,1,0, width-1-i) .. rep3(0,0,val, 1) .. rep3(1,1,0, i)
+
+    data = data .. rep3(1,0,0, width-1-i) .. rep3(0,val,val, 1) .. rep3(1,0,0, i)
+    data = data .. rep3(1,0,0, i)         .. rep3(val,0,val, 1) .. rep3(1,0,0, width-1-i)
+    data = data .. rep3(1,0,0, width-1-i) .. rep3(val,val,0, 1) .. rep3(1,0,0, i)
+
+    data = data .. rep3(0,3,0, width-1-i) .. rep3(v_2,v_2,v_2, 1) .. rep3(0,3,0, i)
+    data = data .. rep3(0,3,0, width-1-i) .. rep3(val,val,val, 1) .. rep3(0,3,0, i)
+    data = data .. rep3(0,3,0, width-1-i) .. rep3(i,i,width-i, 1) .. rep3(0,3,0, i)
+    dev:write(data)
+    socket.sleep(0.02)
+  end
+
+end
+dev:close()
+
